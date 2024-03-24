1575fcbdd601e96b13255a40cb62a04e510447f4
thais-laura <134238333+thais-laura@users.noreply.github.com>
2024-03-24 19:59:34 -0300

    Exemplo + código base

---
+++ funcao_selection_sort.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import numpy as np
+import time
+
+def ordenacao(vetor):
+    ti = time.time()
+    n = len(vetor)
+    for i in range(n):
+        menor_valor = i 
+        for j in range(i + 1, n):
+            if vetor[j] < vetor[menor_valor]:  
+                menor_valor = j
+        vetor[i], vetor[menor_valor] = vetor[menor_valor], vetor[i] 
+    print(vetor)
+    duracao = time.time() - ti
+    return duracao
+
+
+vetor = np.array([])
+print(ordenacao(vetor))
+
